685c37c80b5b01461b7adec27c7adb7eae9efeea
Leo Gonzalez <leoggonzalez@hotmail.com>
2021-09-27 11:58:24 +0200

    created navbar links

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,7 +5,24 @@ export function Navbar(): JSX.Element {
   return (
     <nav className="navbar">
       <Container>
-        <h4>Budget App</h4>
+        <div className="navbar__logo">
+          <a href="/">
+            <h4>Budget App</h4>
+          </a>
+        </div>
+        <div className="navbar__navigation">
+          <ul className="navbar__links">
+            <li className="navbar__link">
+              <a href="/accounts">Accounts</a>
+            </li>
+            <li className="navbar__link">
+              <a href="/income">Income</a>
+            </li>
+            <li className="navbar__link">
+              <a href="/expenses">Expenses</a>
+            </li>
+          </ul>
+        </div>
       </Container>
     </nav>
   );
